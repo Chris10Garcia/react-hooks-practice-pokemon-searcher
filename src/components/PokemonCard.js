4b3bcd0092e73dc39d978e345f93bb214e196192
Christian Garcia <bones192nd@gmail.com>
2023-04-07 16:34:04 -0400

    toggle of picture semi working. need to fix or develop better logic

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
 
+
+
 function PokemonCard({pokemon}) {
   const {id, name, hp, sprites} = pokemon
+  let toggle = true
+  let image = sprites.front
+
+  function handleOnClick(e){
+    image = !toggle ? e.target.src = sprites.back : sprites.back
+    toggle = !toggle
+  }
 
   return (
     <Card>
-      <div>
-        <div className="image">
-          <img src = {sprites.front} alt={name} />
+      <div >
+        <div onClick = {handleOnClick} className="image">
+          <img src = {image} alt={name} />
         </div>
         <div className="content">
           <div className="header">{name}</div>
